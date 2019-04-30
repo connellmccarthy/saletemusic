253f82833a3552e710661013df3f6371a1bfd148
Connell McCarthy <connellmccarthy@gmail.com>
2019-04-30 16:39:10 -0400

    Replace myshopify.com

--- bin/js/shop.js
+++ bin/js/shop.js
@@ -1,5 +1,5 @@
 var dn_bomb_s, dn_bomb_m, dn_bomb_l, dn_bomb_xl, dn_bomb_xxl, dn_hood_s, dn_hood_m, dn_hood_l, dn_hood_xl, dn_hood_xxl, dn_tee_s, dn_tee_m, dn_tee_l, dn_tee_xl, dn_tee_xxl;
-var url = 'https://connellmccarthy.myshopify.com/cart/';
+var url = 'https://shop.connellmccarthy.com/cart/';
 var urlend = ':1?channel=buy_button';
 
 $('.option').click(function(e) {
